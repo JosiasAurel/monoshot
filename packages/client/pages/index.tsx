87863ec24b31eeec549d7fb91657063601961681
JosiasAurel <ndjosiasaurel@gmail.com>
2022-06-22 09:24:15 +0100

    change logo

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -24,14 +24,11 @@ const HomePage: React.FC = (): JSX.Element => {
 				justifyContent: "space-evenly",
 				alignItems: "center"
 			}}>
-				<svg style={{
-					width: "20%",
-					height: "20%",
-					color: "black"
-				}} width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M7 12H18M7 12L5 10H1L3 12L1 14H5L7 12ZM18 12L16 10M18 12L16 14" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
-					<path d="M17.5 22C20.5376 22 23 17.5228 23 12C23 6.47715 20.5376 2 17.5 2C14.4624 2 12 6.47715 12 12C12 17.5228 14.4624 22 17.5 22Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
-				</svg>
+				<h1 style={{
+					fontSize: "5em"
+				}}>
+					💇🏽
+				</h1>
 				<h2 style={{
 					fontSize: "2.8em",
 					textAlign: "center",
