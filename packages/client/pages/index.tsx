cb404518179e2f50a38f82b78f393f3fe1b6b2c2
JosiasAurel <ndjosiasaurel@gmail.com>
2022-06-22 08:42:09 +0100

    change homepage

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -32,40 +32,35 @@ const HomePage: React.FC = (): JSX.Element => {
 					<path d="M7 12H18M7 12L5 10H1L3 12L1 14H5L7 12ZM18 12L16 10M18 12L16 14" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
 					<path d="M17.5 22C20.5376 22 23 17.5228 23 12C23 6.47715 20.5376 2 17.5 2C14.4624 2 12 6.47715 12 12C12 17.5228 14.4624 22 17.5 22Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
 				</svg>
-				<span style={{
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-					justifyContent: "flex-start"
-				}}>
-					<h2 style={{
-						fontSize: "2.8em",
-						textAlign: "center",
-						fontWeight: "bolder",
-						color: "white",
-						backgroundColor: "#8686ff",
-						width: "fit-content",
-						borderBottom: "4px solid black"
-					}}>A profile for every freelancer with</h2>
-					<br />
-					<em style={{
-						fontSize: "2.8em",
-						textAlign: "center",
-						fontWeight: "bolder",
-						color: "white",
-						backgroundColor: "#8686ff",
-						width: "fit-content",
-						borderBottom: "4px solid black",
-					}}>MonoShot</em>
+				<h2 style={{
+					fontSize: "2.8em",
+					textAlign: "center",
+					fontWeight: "bolder",
+					color: "white",
+					backgroundColor: "#8686ff",
+					width: "fit-content",
+					borderBottom: "4px solid black"
+				}}>Yo! Wanna Hair Cut ?</h2>
+				<span>
+					<Link href="/app">
+						<Button style={{
+							margin: "1em 0.5em",
+							padding: "0.5em 1em",
+							boxShadow: "0 0 2px 2px #c2c2c2"
+						}} variant="primary">
+							Browse Haircuts 💇🏽
+						</Button>
+					</Link>
+					<Link href="/app">
+						<Button style={{
+							margin: "1em 0.5em",
+							padding: "0.5em 1em",
+							boxShadow: "0 0 2px 2px #c2c2c2"
+						}} variant="light">
+							I'm a barber 🪒
+						</Button>
+					</Link>
 				</span>
-				<Link href="/app">
-					<Button style={{
-						marginTop: "1em",
-						padding: "0.5em 1em"
-					}} variant="primary">
-						Give it a go 👍🏽
-					</Button>
-				</Link>
 			</main>
 		</div >
 	)
