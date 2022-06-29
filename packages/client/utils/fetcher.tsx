b97c2c0a8a5e5a4cc1cef207f02134053b37e0ff
JosiasAurel <ndjosiasaurel@gmail.com>
2022-06-29 08:06:03 +0100

    add fn to create update barber and get haircuts

--- packages/client/utils/fetcher.tsx
+++ packages/client/utils/fetcher.tsx
@@ -20,6 +20,52 @@ async function makePostRequest(endpoint: string, body): Promise<any> {
     return result;
 }
 
-export {
+async function createBarber(
+    name: string,
+    email: string,
+    phone: string,
+    startingHour: Date,
+    duration: number // hours
+): Promise<any> {
+    const result = await makePostRequest(
+        "/create-barber",
+        {
+            name,
+            email,
+            phone,
+            startingHour,
+            duration
+        }
+    )
 
+    return result;
+}
+
+async function updateBarber(
+    name?: string,
+    email?: string,
+    phone?: string,
+    startingHour?: Date,
+    duration?: number // hours
+): Promise<any> {
+    const result = await makePostRequest("/update-barber", {
+        name: name ? name : undefined,
+        email: email ? email : undefined,
+        phone: phone ? phone : undefined,
+        startingHour: startingHour ? startingHour : undefined,
+        duration: duration ? duration : duration
+    });
+
+    return result;
+}
+
+async function getHaircuts(): Promise<any> {
+    const result = await makeGetRequest("/showcase");
+    return result;
+}
+
+export {
+    createBarber,
+    updateBarber,
+    getHaircuts
 }
